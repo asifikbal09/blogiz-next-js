942e3432278fd72b855f695871f388128df59249
asifikbal09 <asif.ikbal662@gmail.com>
2025-11-29 00:35:32 +0600

    [DONE] Generate static params function

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,15 +1,26 @@
-import BlogDetails from '@/components/ui/BlogDetails';
-import React from 'react';
+import BlogDetails from "@/components/ui/BlogDetails";
+import { Blog } from "@/types";
+import React from "react";
+
+export const generateStaticParams = async () => {
+  const res = await fetch("http://localhost:5000/blogs");
+  const blogs = await res.json();
+  return blogs.slice(0, 3).map((blog: Blog) => ({
+    blogId: blog.id,
+  }));
+};
 
 const BlogDetailsPage = async ({ params }: { params: { blogId: string } }) => {
-    const { blogId } = params;
-    const res = await fetch(`http://localhost:5000/blogs/${blogId}`, { cache: "no-store" });
-    const blog = await res.json();
-    return (
-        <div className="w-[90%] mx-auto">
-            <BlogDetails blog={blog} />
-        </div>
-    );
+  const { blogId } = params;
+  const res = await fetch(`http://localhost:5000/blogs/${blogId}`, {
+    cache: "no-store",
+  });
+  const blog = await res.json();
+  return (
+    <div className="w-[90%] mx-auto">
+      <BlogDetails blog={blog} />
+    </div>
+  );
 };
 
 export default BlogDetailsPage;
